3b2a49850eb453efba68643ff11d361f845ccfb3
unknown <abim7562@gmail.com>
2024-07-31 13:03:03 +0530

    updating env

--- src/Auth/env.js
+++ src/Auth/env.js
@@ -1,4 +1,9 @@
 import io from "socket.io-client";
 
-// export const socket = io("https://quiz-backend-1-5cq2.onrender.com:8000")
-export const socket = io("http://localhost:8000");
+export const socket = io("https://quiz-backend-hczq.onrender.com")
+// export const socket = io("https://quiz-backend-jet.vercel.app");
+
+// export const socket = io("http://localhost:8000")
+
+
+
